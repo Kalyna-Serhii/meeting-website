fcc49c824e1c9870d076d3e3cbc844c76ead9bad
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2024-03-08 23:44:20 +0200

    Fix getting photo from server

--- server/app.js
+++ server/app.js
@@ -5,9 +5,14 @@ import corsOptions from './src/cors/cors.config.js';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './src/middlewares/error-middleware.js';
 import {authRouter, friendRequestRouter, userRouter} from './src/routes/index.js';
+import path from "path";
+import {fileURLToPath} from "url";
 
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -15,6 +20,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use('/api', authRouter, userRouter, friendRequestRouter);
+app.use('/api/photos', express.static(path.join(__dirname, 'photos')));
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
