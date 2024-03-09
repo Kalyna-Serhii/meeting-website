d5f626e4978d97e081ba494cabf1b912dbbed436
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2024-03-09 21:24:04 +0200

    Add Footer Component

---
+++ client/src/components/FooterComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="component">
+    <footer>
+      <ul>
+        <li><a href="mailto:ser.kalyna@gmail.com">Email</a></li>
+        <li><a href="https://github.com/Kalyna-Serhii">GitHub</a></li>
+        <li><a href="https://linkedin.com/in/kalyna-serhii/">LinkedIn</a></li>
+        <li><a href="https:/t.me/Serega_10_04">Telegram</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
